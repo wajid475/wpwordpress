6e4e08d75f329751a04d1183fd00426eec544c6f
wajid475 <145705002+wajid475@users.noreply.github.com>
2025-11-12 18:27:05 +0100

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,21 @@
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = var.vpc_dns_hostnames
+  enable_dns_support   = var.vpc_dns_support
+
+  tags = {
+    "Owner" = var.owner
+    "Name"  = "${var.owner}-vpc"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Owner" = var.owner
+    "Name"  = "${var.owner}-igw"
+  }
+}
